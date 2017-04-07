7d72a592ed2c10165b69461003d29b4bfa57254a
Garrett Serack <garretts@microsoft.com>
2017-04-07 11:43:50 -0700

    Initial Import of code

---
+++ server/interfaces.ts
@@ -0,0 +1,20 @@
+// ---------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License. See License.txt in the project root for license information.
+// ---------------------------------------------------------------------------------------------
+
+// The settings interface describe the server relevant settings part
+export interface Settings {
+  autorest: AutoRestSettings;
+}
+
+// These are the settings we defined in the client's package.json
+// file
+export interface AutoRestSettings {
+  maxNumberOfProblems: number;
+  information: boolean;
+  verbose: boolean;
+  debug: boolean;
+  runtimeId: string;
+  minimumAutoRestVersion: string;
+}
